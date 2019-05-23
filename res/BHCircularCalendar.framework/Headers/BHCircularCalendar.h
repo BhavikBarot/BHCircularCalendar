50ef9fcef2482bfbaa7abedc4d98cc17ad5bf744
Bhavik Barot <39043956+BhavikBarot@users.noreply.github.com>
2019-05-23 14:06:36 +0530

    Add files via upload

---
+++ res/BHCircularCalendar.framework/Headers/BHCircularCalendar.h
@@ -0,0 +1,19 @@
+//
+//  BHCircularCalendar.h
+//  BHCircularCalendar
+//
+//  Created by CIPL-PC68 on 15/03/19.
+//  Copyright © 2019 Bhavik Barot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BHCircularCalendar.
+FOUNDATION_EXPORT double BHCircularCalendarVersionNumber;
+
+//! Project version string for BHCircularCalendar.
+FOUNDATION_EXPORT const unsigned char BHCircularCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BHCircularCalendar/PublicHeader.h>
+
+
